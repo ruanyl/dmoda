7ec3c069b96fe47258275fc60b94f3462785b73d
afarriaga <allan.arriaga@conmio.com>
2014-08-12 21:21:33 +0300

    progress on front end, main layout and index page

--- routes/index.js
+++ routes/index.js
@@ -5,33 +5,24 @@ var pages = JSON.parse(fs.readFileSync("./data/pages.json", "utf8"));
 
 /* GET home page. */
 router.get('/', function(req, res) {
-/*	pages.forEach(function(el, index) {
-    	console.log(el.name);
-	});*/
-	res.render("index", {pages: pages});
+	console.log(pages[0])
+	res.render("index", {pages: pages, primary_content: pages[0].primary_content});
 });
 
 /* GET products page. */
 router.get('/productos', function(req, res) {
-/*	pages.forEach(function(el, index) {
-    	console.log(el.name);
-	});*/
+
 	res.render("products", {pages: pages});
 });
 
 /* GET gallery page. */
 router.get('/galeria', function(req, res) {
-/*	pages.forEach(function(el, index) {
-    	console.log(el.name);
-	});*/
+
 	res.render("gallery", {pages: pages});
 });
 
 /* GET contact page. */
 router.get('/contacto', function(req, res) {
-/*	pages.forEach(function(el, index) {
-    	console.log(el.name);
-	});*/
 	res.render("contact", {pages: pages});
 });
 
