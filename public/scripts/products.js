f1e391877a0caf3c6a745304cab0040518b8eec7
Allan Fernando <Allan Fernando@users.noreply.github.com>
2014-08-17 18:00:21 +0300

    finalized products page with accordion ui

---
+++ public/scripts/products.js
@@ -0,0 +1,9 @@
+(function($){
+	"use strict";
+	$(document).ready(function(){
+		$('.js-collapsible').collapsible('default-open', {
+			arrowRclass: '',
+			arrowDclass: 'open'
+		});
+	});
+})(jQuery);
